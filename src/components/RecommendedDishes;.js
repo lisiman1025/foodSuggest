b43dbda5ae6808e65ef61cfe0f829a60dedb007c
LihengHu <821786679@qq.com>
2023-04-24 19:07:59 +0800

    更改推荐菜图片

--- src/components/RecommendedDishes;.js
+++ src/components/RecommendedDishes;.js
@@ -4,27 +4,11 @@ const RecommendDish = () => {
     const dishes = [
         {
             name: '红烧肉',
-            image: 'https://inews.gtimg.com/newsapp_bt/0/14792476783/1000',
+            image: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2Fbdbceeda-511a-4abc-8843-38ffcfa37721%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1684926343&t=39cf1d1c89a5e2d7216c7ebf4f18c4e5',
         },
         {
             name: '宫保鸡丁',
-            image: 'https://inews.gtimg.com/newsapp_bt/0/14792476783/1000',
-        },
-        {
-            name: '麻婆豆腐',
-            image: 'https://inews.gtimg.com/newsapp_bt/0/14792476783/1000',
-        },
-        {
-            name: '水煮鱼',
-            image: 'https://inews.gtimg.com/newsapp_bt/0/14792476783/1000',
-        },
-        {
-            name: '糖醋排骨',
-            image: 'https://inews.gtimg.com/newsapp_bt/0/14792476783/1000',
-        },
-        {
-            name: '香辣鸭血',
-            image: 'https://inews.gtimg.com/newsapp_bt/0/14792476783/1000',
+            image: 'https://img1.baidu.com/it/u=406735599,2157689115&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=625',
         },
     ];
 
